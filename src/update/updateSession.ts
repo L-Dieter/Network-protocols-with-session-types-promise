3e69fdcb1d68775575113b82d8088efccdab299d
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2025-12-20 22:39:42 +0100

    Update updateSession.ts
    new structure

--- src/update/updateSession.ts
+++ src/update/updateSession.ts
@@ -1,8 +1,10 @@
 import { Session } from "../../protocol";
 import { Marker } from "../interfaces/marker";
+import { getMarker } from "../reference/getMarker";
+import { setMarker } from "../reference/setMarker";
 
 // update the session
-export function updateSession (session: Session, label?: string, marker?: Marker): Session {
+export function updateSession (session: Session, marker: Marker[], label?: string): [Session, Marker[]] {
 
     if (session.kind === "single") {
         session = session.cont;
@@ -11,12 +13,24 @@ export function updateSession (session: Session, label?: string, marker?: Marker
         session = session.alternatives[label];
     }
     else if (session.kind === "def") {
+        setMarker(session.name, marker, { command: "end" }, session);
         session = session.cont;
     }
-    else if (session.kind === "ref" && marker) {
-        session = marker.session;
+    else if (session.kind === "ref") {
+        session = getMarker(session.name, marker).session;
     }
 
-    return session;
+    // if a def or ref is called continue updating the session  
+    while (session.kind === "def" || session.kind === "ref") {
+        if (session.kind === "def") {
+            setMarker(session.name, marker, { command: "end" }, session);
+            session = session.cont;
+        }
+        else if (session.kind === "ref") {
+            session = getMarker(session.name, marker).session;
+        }
+    }
+
+    return [session, marker];
 
 }
