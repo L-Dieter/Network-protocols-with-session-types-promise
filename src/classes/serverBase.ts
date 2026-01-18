d06f35739fa9096d2f35d930f47d602fd6829455
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-01-18 23:42:54 +0100

    Update serverBase.ts
    - reworked server to only contain the relevant information for a channel
    - open a new server in the constructor
    - added a function to check the connection and/or to wait for a client to connect

--- src/classes/serverBase.ts
+++ src/classes/serverBase.ts
@@ -1,73 +1,70 @@
-import { WebSocket, WebSocketServer } from "ws";
+import { WebSocket } from "ws";
 import { Session } from "../../protocol";
 import { Config } from "../../input";
 import { Marker } from "../interfaces/marker";
 import { Channel } from "./channel";
 import { updateSession } from "../update/updateSession";
+import { ServerConnection } from "./serverConnect";
 
 
 // create a new server
 export class Server extends Channel {
 
-    // the server to initialize
-    server: WebSocketServer | null = null;
     // the client that connects to the server
-    client: WebSocket | null = null;
+    socket: WebSocket | null = null;
     // the session given by the config
     session: Session;
+    // the server to initialise in the constructor
+    server: ServerConnection;
 
-    // receive a config with more information about the session
-    // and start a new server
-    constructor (config: Config) {
-
+    constructor(config: Config) {
         super();
 
         this.session = config.session;
-        
-        this.server = new WebSocketServer({port: config.port});
 
-        this.initListeners(config.port);
-        this.initSession();
+        this.server = new ServerConnection(config.port);
+
     }
 
-    // initialize the listeners needed for a proper connection
-    private initListeners(port: number) : void {
+    // check if a client connects and initialize the needed listeners and session afterwards
+    public waitForClientConnection() : Promise<void> {
+        return new Promise((resolve) => {
+            if (this.server.socket?.readyState !== 1) {
+                setTimeout(() => {
+                    resolve(this.waitForClientConnection());
+                }, 100);
+            }
+            else {
+                this.initListeners();
+                this.initSession();
+                resolve();
+            }
+        });
+    }
 
-        // check if a server exists
-        if (!this.server) {
-            throw new Error("No server found");
+    // initialize the listeners for a connected client
+    private initListeners(): void {
+
+        this.socket = this.server.socket;
+
+        if (!this.socket) {
+            return;
         }
 
-        // get a notification when the server is ready
-        this.server.on('listening', () => {
-            console.log(`Listening at ${port}...`);
+        // store the incoming messages in the input array
+        this.socket.on('message', (message: any) => {
+            this.messages.push(message);
+        })
+    
+        // short information if the client disconnects from the server
+        this.socket.on('close', () => {
+            console.log('Client disconnected');
         });
+    }
 
-        // throw an error is something went wrong
-        this.server.on('error', (e) => {
-            console.error(e);
-        });
-
-        // get a notification if the connection gets closed
-        this.server.on('close', () => {
-            console.log("Connection closed");
-        });
-
-        // check for connecting clients
-        this.server.on('connection', (ws: WebSocket) => {
-
-            this.client = ws;
-
-            // store the incoming messages in the input array
-            this.client.on('message', (message: any) => {
-                this.messages.push(JSON.parse(message));
-            })
-
-            // short information if the client disconnects from the server
-            this.client.on('close', () => {
-                console.log('Client disconnected');
-            });
-        });
+    // close the server connection
+    async close() : Promise<void> {
+        this.server.server?.close();
     }
 
     // first check if the session starts with a definition and if so
