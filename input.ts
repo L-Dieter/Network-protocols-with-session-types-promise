ff74f9001c7a9f6f1206e2e4ee8c55990709e9d4
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-01-18 23:27:39 +0100

    Update input.ts
    removed everything related to the type "program"

--- input.ts
+++ input.ts
@@ -1,38 +1,27 @@
 // usage: change the default values with a json
 
-import { Program, Session } from "./protocol";
+import { Session } from "./protocol";
 
 const defaultSession: Session = { kind: 'end' };
-const defaultProgram: Program = { command: 'end' };
 const defaultPort: number = 3000;
 
 export const defaultConfig = {
     session: defaultSession,
-    program: defaultProgram,
     port: defaultPort
 };
 export type Config = {
     session: Session,
-    program: Program,
     port: number
 };
 export function mkConfig(
     session: Session = defaultSession,
-    program: Program = defaultProgram,
     port: number = defaultPort
 ): Config {
-    return { session, program, port };
+    return { session, port };
 }
 export function getSession(config: Config): Session {
     return config.session;
 }
-export function getProgram(config: Config): Program {
-    return config.program;
-}
 export function getPort(config: Config): number {
     return config.port;
 }
-
-
-
-
