303f37aecedf6b655ae0ccef8b20db2eec56549f
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-01-18 23:33:48 +0100

    Update channel.ts
    - slightly reworked the channel
    - added functions to wait for an input

--- src/classes/channel.ts
+++ src/classes/channel.ts
@@ -1,16 +1,16 @@
-import { WebSocket, WebSocketServer } from "ws";
+import { WebSocket } from "ws";
 import { Label, Session } from "../../protocol";
 import { Marker } from "../interfaces/marker";
 import { Choice } from "./choice";
 import { updateSession } from "../update/updateSession";
-import { resolve } from "path";
+import { checkPayload } from "../check/checkPayload";
 
 // create a new channel for a server or client
+// export abstract class Channel {
 export abstract class Channel {
 
     // connection values
-    abstract server: WebSocketServer | null;
-    abstract client: WebSocket | null;
+    abstract socket: WebSocket | null;
 
     // the session to work with on a server
     abstract session: Session;
@@ -28,16 +28,8 @@ export abstract class Channel {
         if (this.session.kind !== "single" || this.session.dir !== "recv") {
             throw new Error(`Invalid method **RECEIVE** for the given session type`);
         }
-      
-        await this.next();
-        const recv: any = this.messages.shift();
 
-        // check if the message can be transformed from JSON
-        try {
-            return JSON.parse(recv);
-        } catch (error) {
-            throw new Error("Not a valid JSON");
-        }
+        return await this.waitForMessage().catch((e) => console.error("Error thrown at .receive(): ", e));
 
     }
 
@@ -49,20 +41,17 @@ export abstract class Channel {
             throw new Error(`Invalid method **SEND** for the given session type`);
         }
 
-        await this.next();
-
-        let msg: string = "";
-
+        this.next();
+        
         // check if the message can be transformed to JSON
         try {
-            msg = JSON.stringify(x);
+            const msg: string = JSON.stringify(x);
+            this.socket?.send(msg);
         } catch (error) {
             throw error;
         }
 
-        if (this.client) {
-            this.client.send(msg);
-        }
+
     }
 
     // try to match the first string of the input array with a tag of the choices
@@ -72,20 +61,9 @@ export abstract class Channel {
         if (this.session.kind !== "choice" || this.session.dir !== "recv") {
             throw new Error(`Invalid method **CHOICE** for the given session type`);
         }
+        
+        return await this.waitForLabel(chs).catch((e) => console.error("Error thrown at .choice(): ", e));
 
-        const label: string | undefined = this.messages.shift();
-
-        await this.next(label);
-
-        // check if a given tag by the input array matches a tag in the choice array
-        for (var ch of chs) {
-            if (ch.tag === label) {
-                return ch.callback();
-            }
-        }
-
-        // throw an exception if there is no match
-        throw new Error("No match in CHOICE found!");
     }
 
     // send a message with the selected tag
@@ -96,11 +74,10 @@ export abstract class Channel {
             throw new Error(`Invalid method **SELECT** for the given session type`);
         }
 
-        await this.next(tag);
+        this.next(tag);
 
-        if (this.client) {
-            this.client.send(JSON.stringify(tag));
-        }
+        this.socket?.send(JSON.stringify(tag));
+
     }
 
     // close the connection
@@ -111,23 +88,86 @@ export abstract class Channel {
             throw new Error(`Invalid method **CLOSE** for the given session type`);
         }
 
-        if (this.server) {
-            this.server.close();
-            return;
-        }
-        else if (this.client) {
-            this.client.close();
-            return;
-        }
-
-        throw new Error("This method is not implemented yet!");
+        this.socket?.close();
     }
 
     // update the session and the markers if needed
-    private async next(label?: Label) : Promise<void> {
+    private next(label?: Label) : void {
         const update: [Session, Marker[]] = updateSession(this.session, this.markerDb, label);
         this.session = update[0];
         this.markerDb = update[1];
+    }
+
+
+    // check the stack of messages and wait until one arrives if needed
+    private waitForMessage() : Promise<any> {
+        return new Promise ((resolve, reject) => {
+            if (this.messages.length === 0) {
+                setTimeout(() => {
+                    resolve(this.waitForMessage());
+                }, 100);
+            }
+            else {
+                const recv: any = this.messages.shift();
+                this.next();
+                
+                
+                // check if the message can be transformed from JSON
+                try {
+                    const msg: any = JSON.parse(recv);
+					
+                    // check if the message is of the correct type
+                    if (this.session.kind === "single") {
+                        if (!checkPayload(msg, this.session.payload)) {
+                            reject(`Payload is not of { type: ${this.session.payload.type} }`);
+                        }
+                    }
+                    resolve(msg);
+                } catch (error) {
+                    throw new Error("Not a valid JSON");
+                }
+
+            }
+        });
+    }
+
+    // check the stack of messages if a label for a given choice is available otherwise wait for one
+    private waitForLabel<T>(chs: Array<Choice<T>>) : Promise<any> {
+        return new Promise((resolve, reject) => {
+            if (this.messages.length === 0) {
+                setTimeout(() => {
+                    resolve(this.waitForLabel(chs));
+                }, 100);
+            }
+            else {
+                const recv: any = this.messages.shift();
+                let label: any;
+
+                try {
+                    label = JSON.parse(recv);
+                    if (this.session.kind === "choice") {
+                        if (!checkPayload(label, { type: "string" })) {
+                            reject("Payload is not of { type: string }");
+                        }
+                    }
+                    
+                } catch (error) {
+                    throw new Error("Not a valid JSON");
+                }
+                this.next(label);
+    
+                // check if a given tag by the input array matches a tag in the choice array
+                for (var ch of chs) {
+                    if (ch.tag === label) {
+                        resolve(ch.callback());
+                    }
+                }
+    
+                // throw an exception if there is no match
+                throw new Error("No match in CHOICE found!");
+    
+            }
+        });
     }
 
 }
