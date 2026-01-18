d0d8126315a8b521502cf2c6b33543d667c3818a
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-01-18 23:55:20 +0100

    Update and rename src/testsuite/summClient.ts to src/testsuite/summTest/summClient.ts
    - moved file to a new folder
    - updated imports
    - reworked the session and the start()-function to work as intended

--- src/testsuite/summTest/summClient.ts
+++ src/testsuite/summTest/summClient.ts
@@ -1,15 +1,14 @@
-import { Config, mkConfig } from "../../input";
-import { Session } from "../../protocol";
-import { Client } from "../classes/clientBase";
+import { Config, mkConfig } from "../../../input";
+import { Session } from "../../../protocol";
+import { Client } from "../../classes/clientBase";
 
 
 const session: Session =
     { kind: "def", name: "sum", cont:
         { kind: "choice", dir: "send", alternatives: {
             "Next": { kind: "single", dir: "send", payload: { type: "number" }, cont: { kind: "ref", name: "sum" }},
-            "Quit": { kind: "single", dir: "recv", payload: { type: "number" }, cont: { kind: "end" }}
-            // "Quit": { kind: "end" }
-        }
+            "Quit": { kind: "end" }
+            }
         }
     }
 
@@ -23,14 +22,12 @@ export class SummationClient extends Client {
         this.numbers = numbers;
     }
 
-    async start() : Promise<number> {
+    async start() : Promise<void> {
         for (let n of this.numbers) {
             await this.select("Next");
             await this.send(n);
         }
         await this.select("Quit");
-        let sum = await this.receive();
         await this.close();
-        return sum;
     }
 }
