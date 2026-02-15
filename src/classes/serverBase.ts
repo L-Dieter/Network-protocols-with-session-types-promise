33391d198119080e602a4d675fd8435acb39aa78
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-02-15 14:30:59 +0100

    Update serverBase.ts
    - some cleanups
    - removed variables "socket" and "session" because of changes in the class "channel"
    - removed variable "server" because of changes in the structure of the class
    - the constructure now get the session and a socket as parameters
    - new method "start" that should be used the same way as a abstract method
    - removed method "waitForClientConnection" because it is not needed
    - removed method "close" because it is not needed
    - removed the check for a socket inside the "initListeners" method
    - moved "initSession" method to the class "channel"

--- src/classes/serverBase.ts
+++ src/classes/serverBase.ts
@@ -1,80 +1,38 @@
 import { WebSocket } from "ws";
 import { Session } from "../../protocol";
-import { Config } from "../../input";
-import { Marker } from "../interfaces/marker";
 import { Channel } from "./channel";
-import { updateSession } from "../update/updateSession";
-import { ServerConnection } from "./serverConnect";
 
 
 // create a new server
 export class Server extends Channel {
 
-    // the client that connects to the server
-    socket: WebSocket | null = null;
-    // the session given by the config
-    session: Session;
-    // the server to initialise in the constructor
-    server: ServerConnection;
-
-    constructor(config: Config) {
+    constructor(session: Session, socket: WebSocket) {
         super();
 
-        this.session = config.session;
+        this.session = session;
+        this.socket = socket;
 
-        this.server = new ServerConnection(config.port);
+        this.initListeners();
+        this.initSession();
 
     }
 
-    // check if a client connects and initialize the needed listeners and session afterwards
-    public waitForClientConnection() : Promise<void> {
-        return new Promise((resolve) => {
-            if (this.server.socket?.readyState !== 1) {
-                setTimeout(() => {
-                    resolve(this.waitForClientConnection());
-                }, 100);
-            }
-            else {
-                this.initListeners();
-                this.initSession();
-                resolve();
-            }
-        });
+    async start() : Promise<any> {
+        throw new Error("This method is not implemented yet: .start()");
     }
 
     // initialize the listeners for a connected client
     private initListeners(): void {
 
-        this.socket = this.server.socket;
-
-        if (!this.socket) {
-            return;
-        }
-
         // store the incoming messages in the input array
-        this.socket.on('message', (message: any) => {
+        this.socket?.on('message', (message: any) => {
             this.messages.push(message);
         })
-    
+
         // short information if the client disconnects from the server
-        this.socket.on('close', () => {
+        this.socket?.on('close', () => {
             console.log('Client disconnected');
         });
     }
 
-    // close the server connection
-    async close() : Promise<void> {
-        this.server.server?.close();
-    }
-
-    // first check if the session starts with a definition and if so
-    // immediately go to the next step
-    private initSession(): void {
-
-        if (this.session.kind === "def") {
-            const update: [Session, Marker[]] = updateSession(this.session, this.markerDb);
-            this.session = update[0];
-            this.markerDb = update[1];
-        }
-    }
 }
