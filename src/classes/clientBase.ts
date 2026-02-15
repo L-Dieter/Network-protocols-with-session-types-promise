4c5379d13d17411096515335327fcd2d4207ae0a
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-02-15 14:20:53 +0100

    Update clientBase.ts
    - some cleanups
    - removed variables "socket" and "session" because of changes in the class "channel"
    - moved method "initSession" into class "channel"

--- src/classes/clientBase.ts
+++ src/classes/clientBase.ts
@@ -1,17 +1,9 @@
 import { WebSocket } from "ws";
 import { Channel } from "./channel";
-import { Session } from "../../protocol";
 import { Config } from "../../input";
-import { Marker } from "../interfaces/marker";
-import { updateSession } from "../update/updateSession";
 
 // create a new client
 export class Client extends Channel {
-
-    // the client we initialize in the constructor
-    socket: WebSocket;
-    // the session given by the config
-    session: Session;
 
     // the uri we need to connect to the server
     private uri: string;
@@ -30,54 +22,41 @@ export class Client extends Channel {
 
 
     // initialize all needed listeners
-    private initListeners(uri: string) : void {
-
-        // check if the client exists
-        if (!this.socket) {
-            throw new Error("No client found");
-        }
+    private initListeners() : void {
 
         // get a notification when the client connects to the server
-        this.socket.on('open', () => {
-            console.log(`[Connected to server with url: ${uri}]`);
+        this.socket?.on('open', () => {
+            console.log(`[Connected to server with url: ${this.uri}]`);
         })
 
         // throws an error
-        this.socket.on('error', (e) => {
+        this.socket?.on('error', (e) => {
             console.error("Connection error: ", e);
         })
 
         // close the connection
-        this.socket.on('close', () => {
+        this.socket?.on('close', () => {
             console.log('Disconnected from WebSocket server');
         })
 
         // check for incoming messages, push them to the array and print them to the console
-        this.socket.on('message', (data: any) => {
+        this.socket?.on('message', (data: any) => {
             this.messages.push(data);
         })
-    }
 
-    // first check if the session starts with a definition and if so
-    // immediately go to the next step
-    private initSession(): void {
-        if (this.session.kind === "def") {
-            const update: [Session, Marker[]] = updateSession(this.session, this.markerDb);
-            this.session = update[0];
-            this.markerDb = update[1];
-        }
+        console.log("----- CLIENT LISTENERS INIT -----");
     }
 
     // checks if the server is ready to connect
     public waitForConnection() : Promise<void> {
         return new Promise((resolve) => {
-            if (this.socket.readyState !== 1) {
+            if (this.socket?.readyState !== 1) {
                 setTimeout(() => {
                     resolve(this.waitForConnection());
                 }, 200);
             }
             else {
-                this.initListeners(this.uri);
+                this.initListeners();
                 this.initSession();
                 resolve();
             }
