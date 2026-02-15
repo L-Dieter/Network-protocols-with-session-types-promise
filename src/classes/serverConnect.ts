35f8cfad01f70e0c1cdd17fb003683f4befd5964
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-02-15 14:38:46 +0100

    Update serverConnect.ts
    - added variables "port", "session", and "channel"
    - the constuctor now takes a config and a type of server as parameters
    - removed the check if a server exists inside the method "initListeners"
    - initialize a new server channel after the connection of a client and start it
    - print out the result of the channel if available

--- src/classes/serverConnect.ts
+++ src/classes/serverConnect.ts
@@ -1,49 +1,63 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { Session } from "../../protocol";
+import { Config } from "../../input";
+import { Server } from "./serverBase";
 
 export class ServerConnection {
 
     // the server to initialize
     public server: WebSocketServer | null = null;
-    // the client that connects to the server
-    public socket: WebSocket | null = null;
+    // the port the server is initialized with
+    private port: number;
+    // the session to define the protocol on this server
+    private session: Session;
+    // the function to initialize a new channel for a client
+    private channel: typeof Server;
 
     // receive a config with more information about the session
     // and start a new server
-    constructor(port: number) {
+    constructor(config: Config, channelType: typeof Server) {
 
-        this.server = new WebSocketServer({ port: port }); 
+        this.port = config.port;
+        this.session = config.session;
 
-        this.initListeners(port);
+        this.server = new WebSocketServer({ port: this.port });
+
+        this.channel = channelType;
+
+        this.initListeners();
     }
 
     // initialize the listeners needed for a proper connection
-    private initListeners(port: number): void {
-
-        // check if a server exists
-        if (!this.server) {
-            throw new Error("No server found");
-        }
+    private initListeners(): void {
 
         // get a notification when the server is ready
-        this.server.on('listening', () => {
-            console.log(`Listening at ${port}...`);
+        this.server?.on('listening', () => {
+            console.log(`Listening at ${this.port}...`);
         });
 
         // throw an error is something went wrong
-        this.server.on('error', (e) => {
+        this.server?.on('error', (e) => {
             console.error(e);
         });
 
         // get a notification if the connection gets closed
-        this.server.on('close', () => {
+        this.server?.on('close', () => {
             console.log("Connection closed");
         });
 
         // check for connecting clients
-        this.server.on('connection', (ws: WebSocket) => {
+        this.server?.on('connection', async (ws: WebSocket) => {
 
-            this.socket = ws;
+            // create a new channel for each client connecting to the server
+            const channel: Server = new this.channel(this.session, ws);
+            const r: any = await channel.start();
+            console.log("RETURN: " + r);
 
         });
+
+        console.log("--- Listeners initialized ---");
+
     }
+
 }
