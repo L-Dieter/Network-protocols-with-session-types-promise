b9a51152eb34e9e7b88b1d893b54a616c6aa771c
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-02-15 14:51:45 +0100

    Update arithServer.ts
    - added parameters to the constructor because of the changes in the superclass
    - changed the methods to use the structure of the arrow function because of problems preserving the ".this"
    - changed the structure of the choices from an array of choices to an object with pairs of a string and a function

--- src/testsuite/arithTest/arithServer.ts
+++ src/testsuite/arithTest/arithServer.ts
@@ -1,6 +1,6 @@
+import { WebSocket } from "ws";
 import { Config, mkConfig } from "../../../input";
 import { Session } from "../../../protocol";
-import { Choice } from "../../classes/choice";
 import { Server } from "../../classes/serverBase";
 
 
@@ -25,39 +25,37 @@ const session: Session =
         }
     };
 
-const config: Config = mkConfig(session);
+export const config: Config = mkConfig(session);
 
 
 export class ArithServer extends Server {
-    constructor() {
+    constructor(session: Session, ws: WebSocket) {
         // super(); // dummy session
-        super(config);
+        super(session, ws);
     }
 
-	async doAdd() : Promise<boolean> {
+	doAdd = async () : Promise<boolean> => {
 		let x1 = await this.receive(); // async method inherited from Server
 		let x2 = await this.receive();
 		await this.send(x1 + x2);
 		return true;
 	}
-	async doNeg() : Promise<boolean> {
+	doNeg = async () : Promise<boolean> => {
 		let x1 = await this.receive();
 		await this.send(-x1);
 		return true;
 	}
-	async doQuit() : Promise<boolean> {
+	doQuit = async () : Promise<boolean> => {
 		return false;
 	}
 
 	async start() {
         let r: boolean;
 		do {
-			r = await this.choice([  // async method inherited from Server
-                //   new Choice("Add", this.doAdd)
-                // , new Choice("Neg", this.doNeg)
-                  new Choice("Add", this.doAdd.bind(this))
-                , new Choice("Neg", this.doNeg.bind(this))
-				, new Choice("Quit", this.doQuit)]);
+			r = await this.choice({  // async method inherited from Server
+                  "Add": this.doAdd
+                , "Neg": this.doNeg
+				, "Quit": this.doQuit});
 	    } while (r);
 		await this.close();
 	}
