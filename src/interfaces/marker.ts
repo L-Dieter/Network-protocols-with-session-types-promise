3882b03ac37580a78951dcba9507ceff08f45f6a
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-03-12 23:27:57 +0100

    Update marker.ts

--- src/interfaces/marker.ts
+++ src/interfaces/marker.ts
@@ -1,5 +1,6 @@
 import { Session } from "../../protocol";
 
+// the interface to store a reference for session together with a name
 export interface Marker {
     name: string,
     session: Session
