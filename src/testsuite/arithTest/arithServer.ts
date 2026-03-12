97e148c86eb2e59b47c681095a1ecefbe4c8d894
L-Dieter <163324047+L-Dieter@users.noreply.github.com>
2026-03-12 23:32:28 +0100

    Update arithServer.ts

--- src/testsuite/arithTest/arithServer.ts
+++ src/testsuite/arithTest/arithServer.ts
@@ -3,7 +3,7 @@ import { Config, mkConfig } from "../../../input";
 import { Session } from "../../../protocol";
 import { Server } from "../../classes/serverBase";
 
-
+// the session for a arithmetic server
 const session: Session =
     { kind: "def", name: "arith", cont:
         { kind: "choice", dir: "recv", alternatives:
@@ -25,30 +25,34 @@ const session: Session =
         }
     };
 
-export const config: Config = mkConfig(session);
+// create the config to initialize a server
+export const configArith: Config = mkConfig(session);
 
-
+// a simple arithmetic server
 export class ArithServer extends Server {
     constructor(session: Session, ws: WebSocket) {
-        // super(); // dummy session
         super(session, ws);
     }
 
+	// add two numbers
 	doAdd = async () : Promise<boolean> => {
 		let x1 = await this.receive(); // async method inherited from Server
 		let x2 = await this.receive();
 		await this.send(x1 + x2);
 		return true;
 	}
+	// negate a number
 	doNeg = async () : Promise<boolean> => {
 		let x1 = await this.receive();
 		await this.send(-x1);
 		return true;
 	}
+	// quit the program
 	doQuit = async () : Promise<boolean> => {
 		return false;
 	}
 
+	// choose the next method and close the connection if finished
 	async start() {
         let r: boolean;
 		do {
